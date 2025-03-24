f913049eab84a2c5f2a5178a081d7e74a9f90097
Nehasree08 <nehasrinivas08@gmail.com>
2025-03-24 22:11:48 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Camera from "./components/Camera";
